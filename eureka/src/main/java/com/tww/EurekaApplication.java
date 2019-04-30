957450fe4960a6267fa103fd5654fff2325eaacf
tangweiwei <weiweitang@shangshibang.com>
2019-04-30 14:44:38 +0800

    spring cloud learn

---
+++ eureka/src/main/java/com/tww/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.tww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by tangweiwei on 2019/4/30.
+ */
+@SpringBootApplication
+public class EurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
---
+++ service1/src/main/java/com/tww/Service1Application.java
@@ -0,0 +1,14 @@
+package com.tww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by tangweiwei on 2019/4/30.
+ */
+@SpringBootApplication
+public class Service1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Service1Application.class, args);
+	}
+}
