f01f779e089e3a7546d8a05626d612e3973d0dbb
tangweiwei <weiweitang@shangshibang.com>
2019-04-30 15:52:52 +0800

    123

--- eureka/src/main/java/com/tww/EurekaApplication.java
+++ eureka/src/main/java/com/tww/EurekaApplication.java
@@ -2,10 +2,12 @@ package com.tww;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Create by tangweiwei on 2019/4/30.
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 	public static void main(String[] args) {
