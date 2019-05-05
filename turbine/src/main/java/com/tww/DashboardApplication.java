9f3bdb869a7e2c9264c867808b6a63fa020ec987
tangweiwei <weiweitang@shangshibang.com>
2019-05-05 16:42:10 +0800

    add service feign and turbine

--- turbine/src/main/java/com/tww/DashboardApplication.java
+++ turbine/src/main/java/com/tww/DashboardApplication.java
@@ -2,30 +2,31 @@ package com.tww;
 
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Create by tangweiwei on 2019/5/5.
  */
-@SpringCloudApplication
-@EnableFeignClients
+@SpringBootApplication
 @EnableHystrixDashboard
-public class Service2Application {
+@EnableTurbine
+public class DashboardApplication {
+
 	@Bean
-	public ServletRegistrationBean getServlet() {
+	public ServletRegistrationBean getServlet(){
 		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
 		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
 		registrationBean.setLoadOnStartup(1);
-		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
 		registrationBean.setName("HystrixMetricsStreamServlet");
 		return registrationBean;
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Service2Application.class, args);
+		SpringApplication.run(DashboardApplication.class, args);
 	}
 }
