d601385e0f12ad16e9a14ce8e0811233a321d7af
tangweiwei <weiweitang@shangshibang.com>
2019-05-08 19:33:21 +0800

    add service gateway

---
+++ gateway/src/main/java/com/tww/GateWayApplication.java
@@ -0,0 +1,14 @@
+package com.tww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by tangweiwei on 2019/5/8.
+ */
+@SpringBootApplication
+public class GateWayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+}
