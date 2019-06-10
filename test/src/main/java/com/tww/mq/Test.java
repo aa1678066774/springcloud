5d1d55e94d5e634e4678f2d31c40f12fe86a9c33
tangweiwei <weiweitang@shangshibang.com>
2019-06-10 17:14:56 +0800

    commit sql

--- test/src/main/java/com/tww/mq/Test.java
+++ test/src/main/java/com/tww/mq/Test.java
@@ -1,10 +1,15 @@
 package com.tww.mq;
 
+import java.sql.SQLOutput;
+
 /**
  * Create by tangweiwei on 2019/5/29.
  */
 public class Test {
 	public static void main(String[] args){
 		System.out.println("hello");
+		System.out.println("");
+		int s=11;
+		System.out.println(s);
 	}
 }
